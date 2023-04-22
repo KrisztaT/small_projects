570d2a16c8db001c08a769d43c13d6cbd9d192a4
KrisztaT <itsforadsmail@gmail.com>
2023-04-22 16:02:24 +1000

    deletion

--- full_stack_app/script.js
+++
@@ -1,7 +0,0 @@
-const fetchData = async () => {
-    const response = await fetch('/api/data');
-    const data = await response.json();
-    document.getElementById('message-container').innerText = data.message;
-};
-
-fetchData();
